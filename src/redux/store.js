965a386f8b039578795bf205864dbfb2e3f0d670
kwolak <kwolak041@gmail.com>
2020-05-18 16:10:08 +0200

    Configure stylelint and Redux DevTools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
 
 const initialState = {};
 const middleware = [thunk];
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default createStore(rootReducer, initialState, applyMiddleware(...middleware));
+export default createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(...middleware)));

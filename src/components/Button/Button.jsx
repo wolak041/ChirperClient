4efd969c648a734ec52582dd8b9413bfdcf083fa
kwolak <kwolak041@gmail.com>
2020-06-02 15:36:20 +0200

    Button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,11 +3,12 @@ import PropTypes from 'prop-types';
 import styles from './Button.module.scss';
 
 const Button = (props) => {
-  const { disabled, classes, attributes } = props;
+  const { disabled, classes, attributes, variant = 'default' } = props;
+  const variantClass = variant === 'outlined' ? styles.outlined : '';
 
   return (
     <button
-      className={`${classes?.root} ${styles.button}`}
+      className={`${classes?.root} ${styles.button} ${variantClass}`}
       onClick={props.handleClick}
       disabled={disabled}
       {...attributes}
@@ -18,8 +19,11 @@ const Button = (props) => {
 Button.propTypes = {
   handleClick: PropTypes.func,
   disabled: PropTypes.bool,
-  classes: PropTypes.object,
-  attributes: PropTypes.object
+  classes: PropTypes.shape({
+    root: PropTypes.string
+  }),
+  attributes: PropTypes.object,
+  variant: PropTypes.oneOf(['default', 'outlined']),
 }
 
 export default Button;
